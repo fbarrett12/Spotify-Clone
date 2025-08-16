f620c7307bef8501e78106c13d2512c1cdf00e9f
FredrickBarrett <f.barrett12@gmail.com>
2025-08-16 18:07:33 -0500

    App Setup
    installed Tailwind for CSS and ShadCN for components

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+   <>
+      <h1 className="text-red-500 text-5xl font-bold underline">
+        Hello
+      </h1>
+
+      <Button variant={"default"}> This is a button </Button>
+   </>
+  )
+}
+
+export default App
