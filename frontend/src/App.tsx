4d447d45cdbd595ee85ff99b87ea7244b62cb9be
FredrickBarrett <f.barrett12@gmail.com>
2025-08-25 01:58:51 -0500

    redoes whatever was undone to the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import { Button } from './components/ui/button';
+
+export default function App() {
+  return (
+    <header>
+      <SignedOut>
+        <SignInButton>
+          <Button>
+            Sign In
+          </Button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  );
+}
