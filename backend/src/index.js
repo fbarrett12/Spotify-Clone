43c806581139e27f1dd69b0e566a8240e41e103d
FredrickBarrett <f.barrett12@gmail.com>
2025-08-26 01:35:03 -0500

    added cloudinary logic and createSong function

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { clerkMiddleware } from '@clerk/express';
+import fileUpload from "express-fileupload";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -13,12 +15,21 @@ import statRoutes from "./routes/stat.route.js";
 
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // to parse req.body
 
 app.use(clerkMiddleware()); // this will add auth to req obj => req.auth.userId
+app.use(fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "tmp"),
+    createParentPath: true,
+    limits: {
+        fileSize: 10 * 1024 * 1024 // 10MB max file size
+    }
+}))
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
@@ -26,6 +37,11 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/songs", songRoutes);
 app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statRoutes);
+
+// error handler
+app.use((err, req, res, next) => {
+    res.status(500).json({ message: process.env.NODE_ENV === "production" ? "Internal server error" : err.message});
+})
 
 
 app.listen(PORT, () => {
